eba56f2492e9bf996b147eae5e2c80bb96d06cef
Ben Hayward <ben@minds.com>
2024-02-26 22:44:51 +0000

    Flip aspect ratio - typo

--- src/components/landing-section/landing-section.tsx
+++ src/components/landing-section/landing-section.tsx
@@ -24,7 +24,7 @@ export default function LandingSection(): ReactNode {
               />            
             </header>
             <p className={bracketDecorationClass}>{'}'}</p>
-          <div className="relative hidden mt-28 ml-20 2xl:block h-[372px] w-[400px] 3xl:h-[600px] 3xl:w-[600px] 4xl:h-[800px] 4xl:w-[800px]">
+          <div className="relative hidden mt-28 ml-20 2xl:block h-[400px] w-[372px] 3xl:h-[600px] 3xl:w-[600px] 4xl:h-[800px] 4xl:w-[800px]">
               <Image src="/abtract-person-laptop.png" alt="abstract person" fill objectFit="rcover"></Image>
             </div>
           </div>
